33d97bf7535fef2257b39fd81fad91754cb36274
10-menachi <bobitlmr188221@spu.ac.ke>
2025-02-04 16:41:00 +0300

    Handle Negative Numbers

--- utils.js
+++ utils.js
@@ -1,21 +1,26 @@
 const is_armstrong = (num) => {
-  const numStr = num.toString();
+  const absNum = Math.abs(num);
+  const numStr = absNum.toString();
   const power = numStr.length;
   return (
     numStr
       .split("")
-      .reduce((acc, digit) => acc + Math.pow(parseInt(digit), power), 0) === num
+      .reduce((acc, digit) => acc + Math.pow(parseInt(digit), power), 0) ===
+    absNum
   );
 };
 
-const is_even = (num) => num % 2 === 0;
+const is_even = (num) => {
+  return Math.abs(num) % 2 === 0;
+};
 
 const is_prime = (num) => {
   if (num < 2) {
     return false;
   }
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
+  const absNum = Math.abs(num);
+  for (let i = 2; i <= Math.sqrt(absNum); i++) {
+    if (absNum % i === 0) {
       return false;
     }
   }
@@ -23,6 +28,9 @@ const is_prime = (num) => {
 };
 
 const is_perfect = (num) => {
+  if (num <= 0) {
+    return false;
+  }
   let sum = 0;
   for (let i = 1; i < num; i++) {
     if (num % i === 0) {
@@ -33,7 +41,8 @@ const is_perfect = (num) => {
 };
 
 const sum_of_digits = (num) => {
-  return num
+  const absNum = Math.abs(num);
+  return absNum
     .toString()
     .split("")
     .reduce((acc, digit) => acc + parseInt(digit), 0);
